014e0787ab9bb3ec7a6b7c00de3069d3cf9280b4
Aslan133 <dkaslanrp@gmail.com>
2019-12-17 20:29:15 +0200

    Pakeitimas

--- GitHubTestavimas/GitHubTestavimas/Program.cs
+++ GitHubTestavimas/GitHubTestavimas/Program.cs
@@ -11,12 +11,26 @@ namespace GitHubTestavimas
          string Zinute { get; set; }
         void SpausdintiZinute();
     }
-
+    class ZinuteKlase : IZinute
+    {
+        public string Zinute { get; set; }
+        public void SpausdintiZinute()
+        {
+            Console.WriteLine("Zinute");
+        }
+    }
     
     class Program
     {
         static void Main(string[] args)
         {
+            List<IZinute> Zinutes = new List<IZinute>();
+            ZinuteKlase zinute = new ZinuteKlase();
+            Zinutes.Add(zinute);
+            foreach (var item in Zinutes)
+            {
+                item.SpausdintiZinute();
+            }
         }
     }
 }
