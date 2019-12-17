5b35e43343cdc2cc5f008543d175fa7c505cb1af
Žydrūnas Grabauskas <55529426+MotorEvil@users.noreply.github.com>
2019-12-17 19:58:34 +0200

    Sukurtas darbas

---
+++ GitHubTestavimas/GitHubTestavimas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTestavimas
+{
+    interface IZinute
+    {
+         string Zinute { get; set; }
+        void SpausdintiZinute();
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
