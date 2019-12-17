e99608ae52ed0992e77eba894e67ce0e0f3e264c
Shincoo <58980386+Shincoo@users.noreply.github.com>
2019-12-17 20:29:01 +0200

    Pridejau zinute

--- GitHubTestavimas/GitHubTestavimas/Program.cs
+++ GitHubTestavimas/GitHubTestavimas/Program.cs
@@ -11,12 +11,30 @@ namespace GitHubTestavimas
          string Zinute { get; set; }
         void SpausdintiZinute();
     }
-
+    class Message : IZinute
+    {
+        public string Zinute { get; set; }
+        public Message(String zinute)
+        {
+            Zinute = zinute;
+        }
+        public void SpausdintiZinute()
+        {
+            Console.WriteLine(Zinute);
+        }
+    }
     
     class Program
     {
         static void Main(string[] args)
         {
+            List<IZinute> Zinutes = new List<IZinute>();
+            Zinutes.Add(new Message("Works"));
+
+            foreach(var item in Zinutes)
+            {
+                item.SpausdintiZinute();
+            }
         }
     }
 }
